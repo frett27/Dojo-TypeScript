34dc35ddcbd4edd9aef35b80d8bc8ad449329bf0
Patrice Freydiere <frett27@gmail.com>
2013-12-11 18:58:41 +0100

    Adding fake definition for compiling some projects

---
+++ fake.d.ts
@@ -0,0 +1,27 @@
+
+declare module "dijit/form/CheckBox"
+{
+	var a : any;
+	export = a;
+}
+
+
+declare module "dojox/gfx" 
+{
+	var a : any;
+	export = a;
+}
+
+declare module "dojo/fx/Toggler" 
+{
+	var a : any;
+	export = a;
+}
+
+declare module "dojox/fx" 
+{
+	var a : any;
+	export = a;
+}
+
+
