07f17e7af5c0afc991c011dda4087b63280a829d
Patrice Freydiere <frett27@gmail.com>
2014-01-07 23:31:47 +0100

    Fix method definition

--- dojo.d.ts
+++ dojo.d.ts
@@ -339,7 +339,7 @@ declare module Dojo
 		hitch<F extends Function>(scope: Object, method: F): F;
 		hitch<F extends Function>(scope: Object, method: string): F;
 
-		hitch(scope: Object, method: string): Function;
+		// hitch(scope: Object, method: string): Function;
 		hitch(scope: Object, method: string, ...v_args:any[]): Function;
 
 		mixin<T extends Object>(dest: T, ...sources: PropertiesMap[]): T;
